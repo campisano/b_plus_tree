effbcec6103cf7522d1ea99caaf8cde6f61bf6a1
campisano <campisano@none.null>
2014-11-21 22:15:35 -0200

    comments updated

--- src/Leaf.cpp
+++ src/Leaf.cpp
@@ -47,6 +47,7 @@ uint32_t Leaf::insert(uint32_t _key, uint64_t _address)
     m_addresses[pos] = _address;
     ++m_count;
 
+    // return minor key (the first one)
     return m_keys[0];
 }
 
@@ -75,7 +76,7 @@ uint32_t Leaf::split(Leaf & _new_leaf)
 
     _new_leaf.m_next_sibling_address = m_next_sibling_address;
 
-    // return new leaf first key
+    // return new leaf minor key (the first one)
     return _new_leaf.m_keys[0];
 }
 
@@ -88,9 +89,11 @@ void Leaf::loadAt(std::fstream & _input_file, std::streampos _position)
 {
     // remember the read file position
     std::streampos r_pos = _input_file.tellg();
+
     // go to asked leaf read position
     _input_file.seekg(_position);
     readToNext(_input_file);
+
     // restore original read position
     _input_file.seekg(r_pos);
 }
@@ -99,9 +102,11 @@ void Leaf::update(std::fstream & _output_file, std::streampos _position)
 {
     // remember the write file position
     std::streampos w_pos = _output_file.tellp();
+
     // go to asked write position
     _output_file.seekp(_position);
     append(_output_file);
+
     // restore original write position
     _output_file.seekp(w_pos);
 }
@@ -109,7 +114,6 @@ void Leaf::update(std::fstream & _output_file, std::streampos _position)
 std::string Leaf::toString()
 {
     std::stringstream ss;
-
     ss << '\t' << '\t' << "count: " << m_count << std::endl;
 
     for(uint16_t pos = 0; pos < m_count; ++pos)
--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,19 @@
 //
 //                               count  key   key  next
 //                               +--------------------+
-//                               |   |  10 |  80 |    |
-//                               | 2 |-----+-----| pnt|
-//                               |   | pnt | pnt |    |
+//                               |   |  10 |  80 |    | (this pnt is actually used
+//                               | 2 |-----+-----| pnt|  to determine if the
+//                               |   | pnt | pnt |    |  node childrens are leaf)
 //                               +--------------------+
-//                                     /     /      \
-//             _______________________/     /        \_____NOT USED_____
+//                                      |     |      \
+//                                      |     |       NOT USED
+//                                       \   /      IN THIS CODE
+//                                        | |       /            \
+//                                 +----------------+             (this is the
+//                                 | index archive  |              original b+tree
+//                                 +----------------+              structure)
+//                                     /     /     \              /
+//             _______________________/     /       \______NOT USED_____
 //            /                            /             IN THIS CODE   \
 //         Leaf 1                        Leaf 2                        -----
 //
@@ -23,7 +30,7 @@
 //          \____________________ \      \
 //                               \ \      |
 //                               +--------------------+
-//                               | real data archive  |
+//                               | final data archive |
 //                               +--------------------+
 */
 
@@ -39,8 +46,6 @@
 #define NK 4  // NUM_OF_KEY_PER_LEAF
 #define BS 60 // BLOCK_SIZE: 2 + (4 + 8) * NK + 8 = 58
 
-#define tmp_num_ceps 23
-
 #include <algorithm>
 #include <cstdlib>
 #include <fstream>
@@ -52,13 +57,12 @@
 
 int main(int _n_args, char ** _v_args)
 {
-    // random unique insertions
+    // gerate random unique keys to insert
     std::list<uint32_t> lst_unique_keys;
-
     {
         uint32_t key;
 
-        while(lst_unique_keys.size() < tmp_num_ceps)
+        while(lst_unique_keys.size() < 23)
         {
             key =  rand() % 100;
 
@@ -69,6 +73,7 @@ int main(int _n_args, char ** _v_args)
         }
     }
 
+    // open the index file, truncating
     std::fstream fs_index ("index.dat", std::fstream::in | std::fstream::out | std::fstream::binary | std::fstream::trunc);
 
     if(!fs_index)
@@ -79,24 +84,31 @@ int main(int _n_args, char ** _v_args)
     }
 
     std::cout << "keys per leaf: " << NK << ", block size: " << BS << std::endl << std::endl;
-    Node initial_root(fs_index, NK, BS);
-    initial_root.append(fs_index);
-    Node * root = &initial_root;
+    Node first_created_node(fs_index, NK, BS);
+
+    // write initial root
+    first_created_node.append(fs_index);
+
+    // root node is, initially, the first created node
+    Node * root = &first_created_node;
 
     std::list<uint32_t>::iterator it;
 
+    // insert all keys with they disk address
     for(it = lst_unique_keys.begin(); it != lst_unique_keys.end(); ++it)
     {
         std::cout << "************************* INSERT: " << (*it) << ", " << 100 - (*it) <<  " *************************" << std::endl;
         root = root->insert((*it), 100 - (*it));
-        std::cout << root->toString() << std::endl;
+        std::cout << root->toString() << std::endl; // print status at every insert
     }
 
+    // print finally index status
     std::cout << std::endl << "RESULT: =======================================================" << std::endl;
     std::cout << root->toString() << std::endl << std::endl;
 
     std::cout << "done." << std::endl;
 
+    // close the index file
     fs_index.close();
 
     return 0;
