c0404445168b9fa5a4875d6ff9c56362ebe5413d
campisano <campisano@none.null>
2014-12-17 00:59:10 -0200

    real functional version

--- src/main.cpp
+++ src/main.cpp
@@ -42,12 +42,6 @@
 #define NK 340  // NUM_OF_KEY_PER_LEAF
 #define BS 4096 // BLOCK_SIZE: 2 + (4 + 8) * 340 + 8 = 4090
 
-//#define NK 4  // NUM_OF_KEY_PER_LEAF
-//#define BS 60 // BLOCK_SIZE: 2 + (4 + 8) * NK + 8 = 58
-
-//#define NK 2  // NUM_OF_KEY_PER_LEAF
-//#define BS 40 // BLOCK_SIZE: 2 + (4 + 8) * NK + 8 = 34
-
 #include <algorithm>
 #include <cstdio>
 #include <cstdlib>
@@ -71,65 +65,84 @@ typedef struct {
 
 } Endereco;
 
+void help(char * _command_name);
+int createIndex(char * _input_data, char * _output_index);
+int findData(char * _input_data, char * _output_index, char * _key);
+
 int main(int _n_args, char ** _v_args)
 {
-    // TODO DEBUG
-    /*
-    // gerate random unique keys to insert
-    std::list<uint32_t> lst_unique_keys;
+    if(_n_args == 4 && (strcmp(_v_args[1], "--create-index") == 0))
     {
-        uint32_t key;
-
-        while(lst_unique_keys.size() < 23)
-        {
-            key =  rand() % 100;
-
-            if(std::find(lst_unique_keys.begin(), lst_unique_keys.end(), key) == lst_unique_keys.end())
-            {
-                lst_unique_keys.push_back(key);
-            }
-        }
+        return createIndex(_v_args[2], _v_args[3]);
     }
-    */
+    else if(_n_args == 5 && (strcmp(_v_args[1], "--find-data") == 0))
+    {
+        return findData(_v_args[2], _v_args[3], _v_args[4]);
+    }
+    else
+    {
+        help(_v_args[0]);
 
+        return 1;
+    }
+}
+
+void help(char * _command_name)
+{
+    std::cerr << "Wrong command" << std::endl;
+    std::cerr << "Usage: " << _command_name << " --create-index <input_data.dat> <output_index.dat>" << std::endl;
+    std::cerr << " Or" << std::endl;
+    std::cerr << "Usage: " << _command_name << " --find-data <input_data.dat> <output_index.dat> <KEY>" << std::endl;
+}
+
+int createIndex(char * _input_data, char * _output_index)
+{
     FILE * cep_file;
     {
-        cep_file = fopen("cep_ordenado_ascii_uniq.dat", "r");
+        cep_file = fopen(_input_data, "r");
 
         if(!cep_file)
         {
-            std::cerr << "Can't open data file " << "cep_ordenado_ascii_uniq.dat" << " for reading." << std::endl;
+            std::cerr << "Can't open data file " << _input_data << " for reading." << std::endl;
 
             return 1;
         }
     }
 
     // open the index file, truncating
-    std::fstream fs_index ("index.dat", std::fstream::in | std::fstream::out | std::fstream::binary | std::fstream::trunc);
+    std::fstream fs_index (_output_index, std::fstream::in | std::fstream::out | std::fstream::binary | std::fstream::trunc);
 
     if(!fs_index)
     {
-        std::cerr << "Can't open index file " << "index.dat" << " for writing." << std::endl;
+        std::cerr << "Can't open index file " << _output_index << " for writing." << std::endl;
 
         return 1;
     }
 
     std::cout << "keys per leaf: " << NK << ", block size: " << BS << std::endl << std::endl;
 
+    // skip initial space to write final root position
+    uint64_t final_root_address = 0;
+    {
+        // write an empty address position
+        fs_index.write((char *)(&final_root_address), sizeof(final_root_address));
+
+        // skip free block space
+        fs_index.seekg(BS - sizeof(final_root_address), fs_index.cur);
+    }
+
     // allocate root node
     Node * root = new Node(fs_index, NK, BS);
 
-    // write initial root
+    // write initial (temporary) root at current position
     root->writeToNext(fs_index);
 
     bool inserted;
     uint32_t new_key;
     uint64_t new_address;
-    //std::list<uint32_t>::iterator it;
     Endereco e;
 
     // insert all keys with they disk address
-    //for(it = lst_unique_keys.begin(); it != lst_unique_keys.end(); ++it)
     do
     {
         new_address = ftell(cep_file);
@@ -142,26 +155,9 @@ int main(int _n_args, char ** _v_args)
         fread(&e.siglaEstado, 1, 2, cep_file);
         fread(&e.cep, 1, 8, cep_file);
         fseek(cep_file, 2, SEEK_CUR);
-/*
-        if(strcmp(e.cep, "20771020") == 0)
-        {
-            printf("\n");
-            printf("Endereco.....: %s\n", e.endereco);
-            printf("Bairro.......: %s\n", e.bairro);
-            printf("Cidade.......: %s\n", e.cidade);
-            printf("Nome Estado..: %s\n", e.nomeEstado);
-            printf("Sigla Estado.: %s\n", e.siglaEstado);
-            printf("CEP..........: %s\n", e.cep);
-            break;
-        }
-*/
-        //new_key = (*it);
+
         new_key = atoi(e.cep);
-        //new_address = 100 - new_key;
 
-        //std::cout << "************************* INSERT: " << new_key << ", " << new_address <<  " *************************" << std::endl;
-
-        // insert
         inserted = root->insert(new_key, new_address);
 
         // if not inserted then node need to be splitted and rotated
@@ -180,39 +176,100 @@ int main(int _n_args, char ** _v_args)
             root = new_root;
         }
 
-        //std::cout << root->toString() << std::endl; // print status at every insert
     } while (strlen(e.cep) > 0);
 
     // print finally index status
-    std::cout << std::endl << "RESULT: =======================================================" << std::endl;
+    //std::cout << std::endl << "RESULT: =======================================================" << std::endl;
     //std::cout << root->toString() << std::endl << std::endl;
 
     std::cout << "root index position: " << root->getIndexPosition() << std::endl;
     std::cout << "done." << std::endl;
 
+    // write final root address at start position
+    final_root_address = root->getIndexPosition();
+    {
+        // rewind to first byte of index file
+        fs_index.seekp(0, fs_index.beg);
 
+        // write final root position
+        fs_index.write((char *)(&final_root_address), sizeof(final_root_address));
+    }
 
-    uint64_t address = root->getAddressForKey(fs_index, 20771020);
+    // free root node memory
+    delete root;
 
-    memset(&e, 0, sizeof(Endereco));
-    fseek(cep_file, address, SEEK_SET);
-    fread(&e.endereco, 1, 79, cep_file);
-    fread(&e.bairro, 1, 73, cep_file);
-    fread(&e.cidade, 1, 26, cep_file);
-    fread(&e.nomeEstado, 1, 22, cep_file);
-    fread(&e.siglaEstado, 1, 2, cep_file);
-    fread(&e.cep, 1, 8, cep_file);
-    fseek(cep_file, 2, SEEK_CUR);
+    // close data file
+    fclose(cep_file);
 
-    printf("\n");
-    printf("Endereco.....: %s\n", e.endereco);
-    printf("Bairro.......: %s\n", e.bairro);
-    printf("Cidade.......: %s\n", e.cidade);
-    printf("Nome Estado..: %s\n", e.nomeEstado);
-    printf("Sigla Estado.: %s\n", e.siglaEstado);
-    printf("CEP..........: %s\n", e.cep);
+    // close the index file
+    fs_index.close();
 
+    return 0;
+}
 
+int findData(char * _input_data, char * _output_index, char * _key)
+{
+    FILE * cep_file;
+    {
+        cep_file = fopen(_input_data, "r");
+
+        if(!cep_file)
+        {
+            std::cerr << "Can't open data file " << _input_data << " for reading." << std::endl;
+
+            return 1;
+        }
+    }
+
+    // open the index file, readonly
+    std::fstream fs_index ("index.dat", std::fstream::in | std::fstream::binary);
+
+    if(!fs_index)
+    {
+        std::cerr << "Can't open index file " << _output_index << " for writing." << std::endl;
+
+        return 1;
+    }
+
+    std::cout << "keys per leaf: " << NK << ", block size: " << BS << std::endl << std::endl;
+
+    // load root position
+    uint64_t final_root_address = 0;
+    {
+        // write an empty address position
+        fs_index.read((char *)(&final_root_address), sizeof(final_root_address));
+    }
+
+    // allocate root node
+    Node * root = new Node(fs_index, NK, BS);
+
+    // load initial root
+    root->loadAt(fs_index, final_root_address);
+    std::cout << "root index position: " << root->getIndexPosition() << std::endl;
+
+    // find input key
+    uint64_t address = root->getAddressForKey(fs_index, atoi(_key));
+
+    Endereco e;
+    {
+        memset(&e, 0, sizeof(Endereco));
+        fseek(cep_file, address, SEEK_SET);
+        fread(&e.endereco, 1, 79, cep_file);
+        fread(&e.bairro, 1, 73, cep_file);
+        fread(&e.cidade, 1, 26, cep_file);
+        fread(&e.nomeEstado, 1, 22, cep_file);
+        fread(&e.siglaEstado, 1, 2, cep_file);
+        fread(&e.cep, 1, 8, cep_file);
+        fseek(cep_file, 2, SEEK_CUR);
+
+        printf("\n");
+        printf("Endereco.....: %s\n", e.endereco);
+        printf("Bairro.......: %s\n", e.bairro);
+        printf("Cidade.......: %s\n", e.cidade);
+        printf("Nome Estado..: %s\n", e.nomeEstado);
+        printf("Sigla Estado.: %s\n", e.siglaEstado);
+        printf("CEP..........: %s\n", e.cep);
+    }
 
     // free root node memory
     delete root;
